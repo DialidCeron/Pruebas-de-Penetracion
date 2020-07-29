0cab2d048935102cc830ee6f179f771138106636
Dialid <dialid.ceron@gmail.com>
2020-07-29 15:35:13 -0500

    Update wget_enlaces.py

--- Tarea 3. Metadatos/wget_enlaces.py
+++ Tarea 3. Metadatos/wget_enlaces.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #Cerón Rodríguez Lezly Dialid
-#Crea un directorio donde descarga los archivos de la lista, crea al final el archivo reporte que contiene los metadatos agrupados
+
 import os
 import re
+import sys 
 
 creators = []
 applications = []
@@ -11,57 +12,69 @@ companies = []
 modifiedby = []
 modifydate = []
 filesname = []
-os.system("mkdir wget")
-os.system("cd wget")
-os.system("for archivo in $(cat ../enlacesdoc); do sudo wget $archivo; done")
-os.system("exiftool . >> salida")
-f = open("salida", "r")
-lines = f.readlines()
 
-for line in lines:
-	if "File Name " in line:
-		if "Zip File Name " not in line:
-			#print(line)
-			filesname.append(line[34:-1])
-	if "Creator" in line:
+if(len(sys.argv) <2 ):
+	print("Se requiere como argumento una lista de enlaces")
+else:
+	archivo=sys.argv[1]
+	arc = open(archivo, "r")
+	lines = arc.readlines()
+	enlaces=open("enlacesdoc","w+")
+	for line in lines:
+		enlaces.write(line)
+	os.system("mkdir wget")
+	os.system("cd wget")
+	os.system("for archivo in $(cat ../enlacesdoc); do sudo wget $archivo; done")
+	os.system("exiftool . >> salida")
+	f = open("salida", "r")
+	lines = f.readlines()
+
+	for line in lines:
+		if "File Name " in line:
+			if "Zip File Name " not in line:
+				#print(line)
+				filesname.append(line[34:-1])
+		if "Creator" in line:
 		#print(line[34:-1])
-		creators.append(line[34:-1]+" Doc: "+filesname[-1])
-	if "Application" in line:
+			creators.append(line[34:-1]+" Doc: "+filesname[-1])
+		if "Application" in line:
 		#print(line[34:-1])
-		applications.append(line[34:-1]+" Doc: "+filesname[-1])
-	if "Company" in line:
+			applications.append(line[34:-1]+" Doc: "+filesname[-1])
+		if "Company" in line:
 		#print(line[34:-1])
-		companies.append(line[34:-1]+" Doc: "+filesname[-1])
-	if "Last Modified By" in line:
+			companies.append(line[34:-1]+" Doc: "+filesname[-1])
+		if "Last Modified By" in line:
 		#print(line[34:-1])
-		modifiedby.append(line[34:-1]+" Doc: "+filesname[-1])
-	if "Modify Date" in line:
-		if "Zip Modify Date" not in line:
+			modifiedby.append(line[34:-1]+" Doc: "+filesname[-1])
+		if "Modify Date" in line:
+			if "Zip Modify Date" not in line:
 			#print(line[34:-1])
-			modifydate.append(line[34:-1]+" Doc: "+filesname[-1])
-reporte= open("reporte","w+")
-reporte.write("Creadores: \n")
-reporte.write("\n")
-for element in creators:
-	reporte.write(element)
+				modifydate.append(line[34:-1]+" Doc: "+filesname[-1])
+	reporte= open("reporte","w+")
+	reporte.write("Creadores: \n")
 	reporte.write("\n")
+	for element in creators:
+		reporte.write(element)
+		reporte.write("\n")
+	reporte.write("\n")
+	reporte.write("Aplicaciones: \n")
+	for element in applications:
+		reporte.write(element)
+		reporte.write("\n")
+	reporte.write("\n")
+	reporte.write("Companies: \n")
+	for element in companies:
+		reporte.write(element)
+		reporte.write("\n")
+	reporte.write("\n")
+	reporte.write("Modificados por: \n")
+	for element in modifiedby:
+		reporte.write(element)
+		reporte.write("\n")
+	reporte.write("\n")
+	reporte.write("Fecha de modificacion: \n")
+	for element in modifydate:
+		reporte.write(element)
+		reporte.write("\n")
 
-reporte.write("Aplicaciones: \n")
-for element in applications:
-	reporte.write(element)
-	reporte.write("\n")
-reporte.write("\n")
-reporte.write("Companies: \n")
-for element in companies:
-	reporte.write(element)
-	reporte.write("\n")
-reporte.write("\n")
-reporte.write("Modificados por: \n")
-for element in modifiedby:
-	reporte.write(element)
-	reporte.write("\n")
-reporte.write("\n")
-reporte.write("Fecha de modificacion: \n")
-for element in modifydate:
-	reporte.write(element)
-	reporte.write("\n")
+	print("reporte generado. Ejecute : 'cat reporte' para ver")
