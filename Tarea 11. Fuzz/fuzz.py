17bb4e8a1f74fa26d2ad5797ee61012e9e070e61
Dialid <dialid.ceron@gmail.com>
2020-07-31 15:06:21 -0500

    Update fuzz.py

--- Tarea 11. Fuzz/fuzz.py
+++ Tarea 11. Fuzz/fuzz.py
@@ -6,6 +6,7 @@ import sys
 
 header = {'User-Agent':'Mozilla/5.0'} 
 #fuzz_url = "http://truerandom.bid/tienda" 
+caracteres = {'\'', '_', '..', '/', '1', '2', '3', '4', '5','6','7','8','9','0','%','*','UNION'}
 archivo=sys.argv[1]
 arc = open(archivo, "r")
 urls = arc.readlines()
@@ -16,3 +17,10 @@ for url in urls:
 		print("URL potencialmente inyectable:" + url)
 	if "Error" in response.text or "Syntax" in response.text:
 		print("URL potencialmente inyectable:" + url)
+	for c in caracteres:
+		response = requests.get(url+c,headers=header) 
+		status = response.status_code 
+		if (status == "200"):
+			print("URL potencialmente inyectable:" + url)
+		if "Error" in response.text or "Syntax" in response.text:
+			print("URL potencialmente inyectable:" + url)
